7b0659de75cde79fcb6392ec3b4b4e74575b933a
shaikhdanish-create <shaikhkingdanish@gmail.com>
2026-02-24 11:23:37 +0530

    added comments

--- User guesses a number between 1–10..py
+++ User guesses a number between 1–10..py
@@ -1,11 +1,16 @@
+# Importing random module to generate a random number
 import random
 
+# Generate a random number between 1 and 10
 number = random.randint(1, 10)
+
+# Take user input and convert it into integer
 guess = int(input("Guess a number between 1 and 10: "))
 
+# Check if user's guess is correct
 if guess == number:
+    # If guess matches the random number
     print("Correct! You win!")
 else:
-
+    # If guess does not match
     print("Wrong! The number was", number)
-
