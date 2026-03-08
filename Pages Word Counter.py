77d77219354c78b595a5e4acc6ef88565a83bce2
danish almer <shaikhkingdanish@gmail.com>
2026-03-08 06:25:24 +0530

    Update Pages Word Counter.py

--- Pages Word Counter.py
+++ Pages Word Counter.py
@@ -5,3 +5,10 @@ word_per_pages = pages * word_per_pages
 print(f"pages = {pages}")
 print(f"word_per_pages = {word_per_pages}")
 print(word_per_pages)
+
+#out put
+Enter number of pages: 5
+Enter words per page: 200
+Pages = 5
+Words per page = 200
+Total words = 1000
