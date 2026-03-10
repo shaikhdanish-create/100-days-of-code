deb4b78d56648f296801187b9da3489bb2b78162
danish almer <shaikhkingdanish@gmail.com>
2026-03-10 14:40:47 +0530

    Update object and class OOP.py

--- object and class OOP.py
+++ object and class OOP.py
@@ -24,3 +24,19 @@ while is_on:
 
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
+
+
+#description
+Displays available coffee options from the menu
+
+Checks if ingredients are sufficient
+
+Handles payment from the user
+
+Prepares the coffee if payment is successful
+
+Shows a report of resources and money
+
+Turns off the machine when requested
+
+This project is useful for learning Object-Oriented Programming (OOP) in Python, modules, and real-world program simulation.
