e142add0a0a2be5482dae4aaec8548dc30adb0d5
danish almer <shaikhkingdanish@gmail.com>
2026-03-19 03:01:59 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+-marks = {}
+x = int(input("enter phy marks: "))
+marks.update({"phy" : x})
+
+y = int(input("enter math marks: "))
+marks.update({"math" : y})
+
+z = int(input("enter bio marks: "))
+marks.update({"bio" : z})
+
+print((marks))
