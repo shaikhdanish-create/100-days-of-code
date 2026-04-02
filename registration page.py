1cfb341b9a9fb4b305c15d94ac384834f0aff901
danish almer <shaikhkingdanish@gmail.com>
2026-04-02 11:53:59 +0530

    Update registration page.py

--- registration page.py
+++ registration page.py
@@ -6,7 +6,7 @@ def format_name(f_name, l_name):
     format_f_name = f_name.title()
     format_l_name = l_name.title()
     result = f"Result: {format_f_name} {format_l_name}"
-    
+      
     return result
 
 print(format_name(input("What is your first name? "),
