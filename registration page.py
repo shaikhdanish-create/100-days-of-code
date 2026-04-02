3bb206db840423d0e989158e933987eb84c5abd5
danish almer <shaikhkingdanish@gmail.com>
2026-04-02 11:54:09 +0530

    Fix formatting of last name in registration page

--- registration page.py
+++ registration page.py
@@ -4,7 +4,7 @@ def format_name(f_name, l_name):
         return 
     
     format_f_name = f_name.title()
-    format_l_name = l_name.title()
+    format_l_name = l_name.title()  
     result = f"Result: {format_f_name} {format_l_name}"
       
     return result
